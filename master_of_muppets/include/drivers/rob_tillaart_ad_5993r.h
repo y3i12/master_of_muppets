3c837aa8d98ed585335f8773419c82d0990e9966
y3i12 <mytos.mail@gmail.com>
2025-08-16 11:26:49 +0200

    Pin configuration
    The current prototype board has DAC0 mapped to Wire2 and DAC1 mapped to Wire1. The schematics were updated to reflect it.
    Added chip select in the firmware for AD5593R using the address pin as a selector.
    Next steps: update Claude's knowledge about it.

--- master_of_muppets/include/drivers/rob_tillaart_ad_5993r.h
+++ master_of_muppets/include/drivers/rob_tillaart_ad_5993r.h
@@ -18,19 +18,21 @@ public:
     typedef uint16_t value_t;
 
     struct initialization_struct_t {
-        initialization_struct_t( TwoWire* the_wire ) : 
-            wire( the_wire )
+        initialization_struct_t( TwoWire* the_wire, uint8_t the_a0_port ) : 
+            wire(    the_wire    ),
+            a0_port( the_a0_port )
         { }
         
         TwoWire* wire;
+        uint8_t  a0_port;
     };
 
     rob_tillaart_ad_5993r( void ) : wire( 0 ), ad5593r( 0x10 ) { }
     
     void initialize( const initialization_struct_t& initialization_struct );
 
-    void enable( void ) { };
-    void disable( void ) { };
+    void enable( void );
+    void disable( void );
 
     void set_channel_value( uint8_t channel_index, value_t value );
     void set_all_channels_same_value( value_t value_for_all_channels );
@@ -38,6 +40,7 @@ public:
 
 protected:
     TwoWire* wire;
+    uint8_t  a0_port;
     AD5593R  ad5593r;
 
     inline value_t dac_value_rescale( value_t value ) { return static_cast< value_t >( static_cast< uint32_t >( value ) * k_max_val / dr_teeth::k_max_value ); }
